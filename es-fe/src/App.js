4147df8946813b9c0ce72b87def735a929514b0b
congthuan-nguyen <congthuan.nguyen1308@gmail.com>
2024-05-13 01:20:37 +0700

    aiii

--- es-fe/src/App.js
+++ es-fe/src/App.js
@@ -1,15 +1,16 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-import UserRouter from './components/user/UserRouter';
-import "./styles/main.css"
-
+import UserRouter from "./components/user/UserRouter";
+import "./styles/main.css";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/*' index element={<UserRouter />}></Route>
+          <Route path="/*" index element={<UserRouter />}>
+            a
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
