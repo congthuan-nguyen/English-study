5579752cac10542ab1eef30273f3cbb2aed2d880
congthuan-nguyen <congthuan.nguyen1308@gmail.com>
2024-06-14 22:53:09 +0700

    initialization back end english study

---
+++ es_be/src/main/java/es_study/es_be/EsBeApplication.java
@@ -0,0 +1,13 @@
+package es_study.es_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsBeApplication.class, args);
+	}
+
+}
---
+++ es_be/src/test/java/es_study/es_be/EsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package es_study.es_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
